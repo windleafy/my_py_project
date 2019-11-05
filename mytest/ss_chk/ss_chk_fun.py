778617e2549af420d0173966eeedacf38d10169d
windleafy <ydfwind@gmail.com>
2019-11-05 14:47:55 +0800

    create ss gui json

--- mytest/ss_chk/ss_chk_fun.py
+++ mytest/ss_chk/ss_chk_fun.py
@@ -12,24 +12,32 @@ def verify(host, port, passwd, method, local_port):
               'password': passwd, 'method': method,
               'local_address': '127.0.0.1',
               'local_port': local_port, 'timeout': 60}
-    cfile = '/etc/shadowsocks-libev/local_port.json'
+
+    # cfile = '/etc/shadowsocks-libev/%s.json' % local_port
+    cfile = '/etc/shadowsocks-libev/local.json'
+    # print(cfile)
     with open(cfile, 'w') as f:
         json.dump(config, f)
 
-    os.system('systemctl restart shadowsocks-libev-local@local_port.service')
-    print('start server')
+    os.system('systemctl restart shadowsocks-libev-local@local.service')
+    print('server start')
 
     time.sleep(1)
+    # local_port should be int
     h = httplib2.Http(proxy_info=httplib2.ProxyInfo(2, '127.0.0.1', local_port), timeout=60)
+
     status = False
+
     try:
         if v6:
             r, c = h.request('http://v6.ipv6-test.com/api/myip.php')
         else:
-            r, c = h.request('http://httpbin.org/ip')
+            # r, c = h.request('http://httpbin.org/ip')
+            r, c = h.request('http://google.com')
         status = r['status'] == '200'
     except Exception as e:
         print(e)
-    os.system('systemctl stop shadowsocks-libev-local@%s.service' % local_port)
-    os.remove(cfile)
+
+    os.system('systemctl stop shadowsocks-libev-local@local.service')
+    print(f'status{status}')
     return status
---
+++ mytest/ss_chk/ss_chk_run.py
@@ -0,0 +1,82 @@
+from ss_chk.ss_chk_fun import verify
+import json
+
+
+# 取ss-free服务器列表
+def get_server_url():
+    with open('url_list.txt', encoding='utf8') as f:
+        s_content = f.read()
+    content_list = s_content.split('\n')
+    for i in content_list:
+        yield i.split()[1:5]
+
+
+# 验证ss-free服务器列表
+def chk_url(urls):
+    dict_list = []
+    for item in urls:
+        url_dict = {
+            'server': item[0],
+            'server_port': item[1],
+            'password': item[3],
+            'method': item[2],
+            'plugin': '',
+            'plugin_opts': '',
+            'plugin_args': '',
+            'remarks': '',
+            'timeout': 5
+        }
+
+        dict_list.append(url_dict)
+
+    # print(dict_list)
+    '''
+    for i in dict_list:
+        item = i
+        host = item['server']
+        port = item['server_port']
+        passwd = item['password']
+        method = item['method']
+
+        status = verify(host, port, passwd, method, 1080)
+        if status:
+            print(item)
+            yield item
+
+        # print('\n')
+    '''
+    # 待处理为有效server
+    return dict_list
+
+
+# 刷新客户端配置文件
+def refresh_client_json():
+    # 读取gui客户端配置文件
+    file = 'gui-config.json'
+    with open(file, encoding='utf8') as f:
+        c_content = json.load(f)
+    # print(c_content['configs'])
+    print('ss gui client 配置文件读取完毕')
+
+    # 客户端文件刷新
+    c_content['configs'] = s_info_list['config']
+    print('url信息更新完毕')
+
+    # 写入新的gui客户端配置文件
+    file = 'gui-config.json'
+    with open(file, 'w', encoding='utf8') as f:
+        json.dump(c_content, f)
+    print('ss gui client 配置文件重写完毕')
+
+
+if __name__ == '__main__':
+    # 取出全部url
+    url = get_server_url()
+
+    # 取出有效的url
+    s_info_list = {'config': list(chk_url(url))}
+    print('free-ss url获取完毕')
+
+    # 将有效的url存入客户端配置
+    refresh_client_json()
+
