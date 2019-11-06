f426794a121b8c606c98f5fb343c0abe80549c3d
windleafy <ydfwind@gmail.com>
2019-11-06 10:03:39 +0800

    minor modify

--- mytest/test_pypi/py_lib_requests/requests_proxy.py
+++ mytest/test_pypi/py_lib_requests/requests_proxy.py
@@ -14,10 +14,11 @@ proxies = {
            "http": "http://10.10.1.10:3128",
            "https": "http://10.10.1.100:4444",
           }
-r = requests.get('http://m.ctrip.com', proxies=proxies)
+r1 = requests.get('http://m.ctrip.com', proxies=proxies)
 
 # 如果代理需要用户名和密码，则需要这样：
 proxies = {
     "http": "http://user:pass@10.10.1.10:3128/",
 }
-r = requests.get('http://m.ctrip.com', proxies=proxies)
+r2 = requests.get('http://m.ctrip.com', proxies=proxies)
+
