d129d0b46ec53544f18eb57a75012943482f45cd
windleafy <ydfwind@gmail.com>
2019-11-06 10:57:04 +0800

    support url

--- mytest/base_kit/base_ip_chk.py
+++ mytest/base_kit/base_ip_chk.py
@@ -8,9 +8,11 @@
 import socket
 
 
+# 检验指定ip与端口是否能连通，host兼容IP与url
 def check_ip_port(host, port):
+    ip = socket.getaddrinfo(host, None)[0][4][0]
     socket.setdefaulttimeout(3)
-    if ':' in host:
+    if ':' in ip:
         inet = socket.AF_INET6
     else:
         inet = socket.AF_INET
@@ -18,3 +20,9 @@ def check_ip_port(host, port):
     status = sock.connect_ex((host, port))
     sock.close()
     return status == 0
+
+
+if __name__ == "__main__":
+    print(check_ip_port('www.baidu.com', 80))
+    # 测试本地记得开http server
+    print(check_ip_port('127.0.0.1', 80))
