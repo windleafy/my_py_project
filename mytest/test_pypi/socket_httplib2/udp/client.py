1852335ebff074a2f214c66505d67d783b07a10d
windleafy <ydfwind@gmail.com>
2019-12-25 10:56:30 +0800

    p2p通讯模型

---
+++ mytest/test_pypi/socket_httplib2/udp/client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : Shark
+"""
+client.py
+"""
+# @Software: PyCharm
+# @Time    : 2019 12 25 10:16
+
+import socket
+
+# 环境初始化
+port = 8081
+host = 'localhost'
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+while True:
+    # 输入待发送的消息
+    msg = input('说点什么吧：',)
+    msg = msg.encode('utf-8')
+
+    # 发送消息
+    s.sendto(msg, (host, port))
+    print('消息已发送！')
---
+++ mytest/test_pypi/socket_httplib2/udp/server.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : Shark
+"""
+server.py
+"""
+# @Software: PyCharm
+# @Time    : 2019 12 25 10:16
+
+import datetime
+import socket
+
+# 环境初始化
+port = 8081
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(('', port))
+
+while True:
+    # 接收消息
+    data, addr = s.recvfrom(3000)
+
+    # 当前时间
+    rec_time = datetime.datetime.now().strftime('%T')
+
+    # 消息解码
+    data = data.decode('utf-8')
+
+    # 打印消息
+    print(rec_time, 'Received: ', data, ' from ', addr)
